1b97d03b906f4707c6371b43af7d4befda38410a
TerrysGitHubAccount <tas207@zips.uakron.edu>
2019-08-08 10:52:09 -0600

    added code

--- terry.py
+++ terry.py
@@ -1,1 +1,12 @@
-print("testing")
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    greeting = "hello, {}".format(who_to_greet)
+    return greeting
