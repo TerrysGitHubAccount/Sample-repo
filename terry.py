6793def93d2de93923fdac1224c7bc887e98a900
TerrysGitHubAccount <tas207@zips.uakron.edu>
2019-08-09 17:01:51 -0600

    Basic motion controls were added to a circle

--- terry.py
+++ terry.py
@@ -1,12 +1,41 @@
-import math
-import sys
-from os import rename
+import pygame
 
-import requests
+pygame.init()
 
-print(sys.version)
-print(sys.executable)
+window_width = 960
+window_height = 540
+window_dimensions = (window_width, window_height)
+window = pygame.display.set_mode(window_dimensions)
 
-def greet(who_to_greet):
-    greeting = "hello, {}".format(who_to_greet)
-    return greeting
+pygame.display.set_caption("Pygame Differential Drive Robot")
+
+start_x_position = (480)
+start_y_position = (270)
+car_radius = 20
+car_color = (255, 255, 255) #White
+car_velocity = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    #know how the grid works
+    if keys[pygame.K_LEFT]:
+        start_x_position = start_x_position - car_velocity
+    if keys[pygame.K_RIGHT]:
+        start_x_position = start_x_position + car_velocity
+    if keys[pygame.K_UP]:
+        start_y_position = start_y_position - car_velocity
+    if keys[pygame.K_DOWN]:
+        start_y_position = start_y_position + car_velocity
+    
+    window.fill((0,0,0)) #fills black surface refresh
+    pygame.draw.circle(window, car_color,(start_x_position,start_y_position),car_radius)
+    pygame.display.update()
+
+pygame.quit()
